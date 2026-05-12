f9fabd01fc18c07666711746ad97a3ccb8d4004d
UMCA\fsalazarmo <fsalazarmo@castrocarazo.ac.cr>
2026-05-12 12:27:30 -0600

    cambios

--- Backend/Controllers/UsersController.cs
+++ Backend/Controllers/UsersController.cs
@@ -68,11 +68,10 @@ namespace Backend.Controllers
         {
             try
             {
-                string hashString;
+                byte[] hashBytes;
                 using (var sha256 = System.Security.Cryptography.SHA256.Create())
                 {
-                    var hashedBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(request.Password));
-                    hashString = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+                    hashBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(request.Password));
                 }
 
                 using (var command = _dbContext.Database.GetDbConnection().CreateCommand())
@@ -80,7 +79,7 @@ namespace Backend.Controllers
                     command.CommandText = "INSERT INTO cert_usuarios (username, password_hash, rol_id) VALUES (@Username, @Hash, @RolId)";
 
                     var pUser = command.CreateParameter(); pUser.ParameterName = "@Username"; pUser.Value = request.Username; command.Parameters.Add(pUser);
-                    var pHash = command.CreateParameter(); pHash.ParameterName = "@Hash"; pHash.Value = hashString; command.Parameters.Add(pHash);
+                    var pHash = command.CreateParameter(); pHash.ParameterName = "@Hash"; pHash.Value = hashBytes; command.Parameters.Add(pHash);
                     var pRol = command.CreateParameter(); pRol.ParameterName = "@RolId"; pRol.Value = request.RolId; command.Parameters.Add(pRol);
 
                     if (_dbContext.Database.GetDbConnection().State != System.Data.ConnectionState.Open)
